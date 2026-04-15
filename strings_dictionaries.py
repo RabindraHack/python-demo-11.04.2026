47b76bdf2964888a4b8e61f267b9294e94981ee5
Rabindra <rabisahoo151@gmail.com>
2026-04-16 00:54:05 +0530

    odd number check added.

--- strings_dictionaries.py
+++ strings_dictionaries.py
@@ -30,6 +30,18 @@ city = user.get('address', {}).get('city', 'Not Found')
 
 print("City:", city)
 
+# Function to find odd numbers in a list
+def find_odd_numbers(numbers):
+    """
+    This function takes a list of numbers and returns a list of odd numbers.
+    """
+    return [num for num in numbers if num % 2 != 0]
+
+# Example usage
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_numbers = find_odd_numbers(numbers)
+print("Odd numbers:", odd_numbers)
+
 
 
 
